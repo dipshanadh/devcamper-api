71a5fd535793eac5aecc1a99b8b90a50f95f558e
dipshanadh <dipshanadhikari1234@gmail.com>
2022-06-14 21:30:39 +0545

    GeoJSON location - geocoder

--- config/db.js
+++ config/db.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI)
+	const conn = await mongoose.connect(process.env.MONGO_URI)
 
-		console.log(`MongodDB connected: ${conn.connection.host}`)
-	} catch (err) {
-		console.log("Could not connect to MongoDB !")
-	}
+	console.log(`MongodDB connected: ${conn.connection.host}`)
 }
 
 module.exports = connectDB
