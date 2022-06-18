eecd8c3d76e53d8710763b0773cec96e958b0f37
dipshanadh <dipshanadhikari1234@gmail.com>
2022-06-18 13:41:48 +0545

    seeder bug fixed

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: "./config/config.env" })
 
 // Load models
 const Bootcamp = require("./models/Bootcamp")
+const Course = require("./models/Course")
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
@@ -15,16 +16,21 @@ mongoose.connect(process.env.MONGO_URI)
 const bootcamps = JSON.parse(
 	fs.readFileSync(`${__dirname}\\_data\\bootcamps.json`, "utf-8")
 )
+const courses = JSON.parse(
+	fs.readFileSync(`${__dirname}\\_data\\courses.json`, "utf-8")
+)
 
 // Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps)
+		await Course.create(courses)
 
 		console.log("Data Imported....")
 		process.exit()
 	} catch (err) {
-		console.log(err)
+		console.log(err.message)
+		process.exit()
 	}
 }
 
@@ -32,11 +38,13 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany()
+		await Course.deleteMany()
 
 		console.log("Data Destroyed....")
 		process.exit()
 	} catch (err) {
-		console.log(err)
+		console.log(err.message)
+		process.exit()
 	}
 }
 
