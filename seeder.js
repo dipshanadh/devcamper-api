a6753dc61152d8f8aed19c3b173bace1a661734a
dipshaadh <dipshanadhikari1234@gmail.com>
2022-07-17 15:48:10 +0545

    update seeder

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: "./config/config.env" })
 const Bootcamp = require("./models/Bootcamp")
 const Course = require("./models/Course")
 const User = require("./models/User")
+const Review = require("./models/Review")
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
@@ -24,6 +25,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
 	fs.readFileSync(path.join(__dirname, "/_data/users.json"), "utf-8")
 )
+const reviews = JSON.parse(
+	fs.readFileSync(path.join(__dirname, "/_data/reviews.json"), "utf-8")
+)
 
 // Import into DB
 const importData = async () => {
@@ -31,6 +35,7 @@ const importData = async () => {
 		await User.create(users)
 		await Bootcamp.create(bootcamps)
 		await Course.create(courses)
+		await Review.create(reviews)
 
 		console.log("Data Imported....")
 		process.exit()
@@ -46,6 +51,7 @@ const deleteData = async () => {
 		await User.deleteMany()
 		await Bootcamp.deleteMany()
 		await Course.deleteMany()
+		await Review.deleteMany()
 
 		console.log("Data Destroyed....")
 		process.exit()
