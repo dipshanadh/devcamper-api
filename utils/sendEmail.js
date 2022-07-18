62fe7ffd53299a110486069bb536e6ffbaec139e
dipshaadh <dipshanadhikari1234@gmail.com>
2022-07-18 15:42:57 +0545

    send email bug fixed

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,7 +6,7 @@ const sendEmail = async options => {
 		host: process.env.SMTP_HOST,
 		port: process.env.SMTP_PORT,
 		auth: {
-			user: process.env.SMTP_USERNAME,
+			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASSWORD,
 		},
 	})
