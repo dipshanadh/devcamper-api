9de0c85b3c3e3a9bf7bf6ddd3f40c0e49b7b5005
dipshaadh <dipshanadhikari1234@gmail.com>
2022-07-18 14:47:21 +0545

    rate limiting and http param pollution

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv")
 const mongoSanitize = require("express-mongo-sanitize")
 const helmet = require("helmet")
 const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+const hpp = require("hpp")
 
 const connectDB = require("./config/db")
 
@@ -35,6 +37,17 @@ app.use(logger)
 
 // set security headers
 app.use(helmet())
+
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 60 * 1000, // 10mins
+	max: 100,
+})
+
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
 
 // Set static folder
 app.use(express.static("public"))
